9567634dc57debf409ea718693f4c7205c5e7740
Gus963 <gustavo.fernandes66@hotmail.com>
2023-09-21 22:05:58 -0300

    feat: add python codes

---
+++ Códigos/uri_1013.py
@@ -0,0 +1,10 @@
+
+a, b, c = input().split(" ")
+a = int(a)
+b = int(b)
+c = int(c)
+
+maior_ab = (a+b+abs(a-b)) // 2
+maior_ab_2 = (maior_ab+c+abs(maior_ab-c)) // 2
+
+print(f"{maior_ab_2} eh o maior")
